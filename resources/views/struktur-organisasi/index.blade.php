8141197de0effa944fe1b9f8be31b1e705f56cd6
Bayu <bayurz1001@gmail.com>
2025-11-05 23:20:36 +0700

    fix struktur modal

--- resources/views/struktur-organisasi/index.blade.php
+++ resources/views/struktur-organisasi/index.blade.php
@@ -4,7 +4,7 @@
     <div class="space-y-6">
         <livewire:struktur.struktur-list />
         <livewire:struktur.struktur-form />
-        <livewire:struktur.struktur-modal />
+        {{-- <livewire:struktur.struktur-modal /> --}}
 
     </div>
 @endsection
