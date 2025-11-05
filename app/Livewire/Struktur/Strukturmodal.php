4c03cde5002924cff8af7cb09bc410d901e1db64
Bayu <bayurz1001@gmail.com>
2025-11-05 23:18:52 +0700

    fix struktur modal

--- app/Livewire/Struktur/Strukturmodal.php
+++ app/Livewire/Struktur/Strukturmodal.php
@@ -6,6 +6,20 @@ use Livewire\Component;
 
 class StrukturModal extends Component
 {
+    public bool $showModal = false;
+
+    protected $listeners = ['openStrukturModal' => 'openModal'];
+
+    public function openModal(): void
+    {
+        $this->showModal = true;
+    }
+
+    public function closeModal(): void
+    {
+        $this->showModal = false;
+    }
+
     public function render()
     {
         return view('livewire.struktur.struktur-modal');
