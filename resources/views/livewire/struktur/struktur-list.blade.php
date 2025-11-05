634cbb3899cddb4fed7a426d3b7d71ed9865ea77
Bayu <bayurz1001@gmail.com>
2025-11-05 22:29:12 +0700

    fix struktur list

--- resources/views/livewire/struktur/struktur-list.blade.php
+++ resources/views/livewire/struktur/struktur-list.blade.php
@@ -113,7 +113,7 @@
                         <!-- FOTO -->
                         <td class="px-6 py-5 whitespace-nowrap">
                             <div class="w-12 h-12 rounded-xl overflow-hidden shadow-md ring-1 ring-black/5">
-                                <img src="{{ asset($item->foto) }}" alt="Foto {{ $item->nama }}"
+                                <img src="{{ asset('public/storage/' . $item->foto) }}" alt="Foto {{ $item->nama }}"
                                     class="w-full h-full object-cover">
 
                             </div>
