50ea8256ed465326b3d779023475cbe82b2274bd
Bayu <bayurz1001@gmail.com>
2025-11-05 22:16:10 +0700

    fix struktur list

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => ('app/public/storage/'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
