bf6f4f24f9ca3a2ff16cd04f7035d23195b2dc36
Bayu <bayurz1001@gmail.com>
2025-11-05 21:04:17 +0700

    fix index

--- resources/views/struktur-organisasi/index.blade.php
+++ resources/views/struktur-organisasi/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="space-y-6">
-        <livewire:struktur-organisasi.struktur-list />
-        <livewire:struktur-organisasi.struktur-form />
+        <livewire:struktur.struktur-list />
+        <livewire:struktur.struktur-form />
         {{-- <livewire:sejarah.sejarah-modal /> --}}
     </div>
 @endsection
