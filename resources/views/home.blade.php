2f06f3f06d9849a288f265aa1db07da3002c1f23
Bayu <bayurz1001@gmail.com>
2025-11-09 00:37:05 +0700

    feat: adjust pagination settings for announcement modes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -509,6 +509,11 @@
 
         {{-- pengumuman content hub --}}
         @livewire('content.content-hub', ['mode' => 'announcement', 'showPagination' => false])
+        @if ($showPagination && $items->hasPages())
+            <div class="pt-4 flex justify-center">
+                {{ $items->onEachSide(1)->links() }}
+            </div>
+        @endif
 
 
         <!-- Tombol -->
