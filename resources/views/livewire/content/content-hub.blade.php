2261015e9fa190275db85e456430174ba8a41f62
Bayu <bayurz1001@gmail.com>
2025-11-09 01:13:12 +0700

    feat: adjust pagination settings

--- resources/views/livewire/content/content-hub.blade.php
+++ resources/views/livewire/content/content-hub.blade.php
@@ -14,9 +14,5 @@
         @break
     @endswitch
 
-    @if ($showPagination && $items->hasPages())
-        <div class="pt-2 flex justify-center">
-            {{ $items->onEachSide(1)->links() }}
-        </div>
-    @endif
+
 </div>
