8ec60a857c9891ff8fb277d6cad90e8eaa6693da
Bayu <bayurz1001@gmail.com>
2025-11-11 21:52:34 +0700

    feat: adjust news

--- resources/views/potensidesa.blade.php
+++ resources/views/potensidesa.blade.php
@@ -81,12 +81,14 @@
                                 </div>
 
                                 <h2
-                                    class="mt-3 text-base md:text-lg font-semibold text-gray-900 line-clamp-2 transition-colors duration-200 group-hover:text-green-700">
+                                    class="mt-3 text-xl md:text-2xl font-semibold text-gray-900 line-clamp-2  transition-colors duration-200 group-hover:text-green-700">
                                     {{ $featured->title }}
                                 </h2>
-                                <p class="mt-2 text-gray-700 line-clamp-3">
+
+                                <p class="mt-2 text-xs md:text-sm text-gray-700 leading-snug line-clamp-3">
                                     {{ $featDesc }}
                                 </p>
+
 
                                 <dl class="mt-3 grid grid-cols-2 gap-2 text-xs text-gray-600">
 
