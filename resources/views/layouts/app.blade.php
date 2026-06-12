0763dd2e0a276530b18b5e1365d4f55e7eaa7b8d
Bayu <bayurz1001@gmail.com>
2026-06-12 12:15:52 +0700

    feat:  favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('public/favicon.ico') }}">
     <title>{{ config('app.name', 'SatuDesa') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
